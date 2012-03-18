b36454f670d2933eaa34f541256d58270d2e1512
Maciej Sawicki <maciej.sawicki@polidea.pl>
2012-03-18 15:59:48 +0100

    -selecting stage descriptions -disabled buttons for disabled tracks

--- src/pl/org/kopernik/app/KopernikActivity.java
+++ src/pl/org/kopernik/app/KopernikActivity.java
@@ -11,29 +11,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class KopernikActivity extends Activity {
 	Context ctx;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        ctx = this;
-        
-       /* ArrayList<String> answers = new ArrayList<String>();
-        answers.add("pierwsza odp");
-        answers.add("druga odp");
-        answers.add("trzecia odp");
-        Quize q = new Quize("przykladowe pytanie", answers, 0);
-        Exhibit e = new Exhibit(0, 0, "eksponat 1", "idz prosto do wielkiego kola", "dlugi opis jaka piekna rzecz", q, 0);
-        pl.org.kopernik.json.Path p = new pl.org.kopernik.json.Path();
-        p.addExhibit(e);
-        Log.d("JSON", p.returnAaJSON().toString());*/
-    }
-    
-    public void goToExhibit(View v) {
-    	Intent i = new Intent(ctx, PathActivity.class);
-    	startActivity(i);
-    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		ctx = this;
+
+		/*
+		 * ArrayList<String> answers = new ArrayList<String>();
+		 * answers.add("pierwsza odp"); answers.add("druga odp");
+		 * answers.add("trzecia odp"); Quize q = new
+		 * Quize("przykladowe pytanie", answers, 0); Exhibit e = new Exhibit(0,
+		 * 0, "eksponat 1", "idz prosto do wielkiego kola",
+		 * "dlugi opis jaka piekna rzecz", q, 0); pl.org.kopernik.json.Path p =
+		 * new pl.org.kopernik.json.Path(); p.addExhibit(e); Log.d("JSON",
+		 * p.returnAaJSON().toString());
+		 */
+	}
+
+	public void goToExhibit(View v) {
+		Intent i = new Intent(ctx, PathActivity.class);
+		Button b = (Button) v;
+		int id = b.getId();
+		if (id == R.id.water) {
+			i.putExtra("path", "water");
+		} else if (id == R.id.air) {
+			i.putExtra("path", "air");
+		} else if (id == R.id.fire) {
+			i.putExtra("path", "fire");
+		} else if (id == R.id.earth) {
+			i.putExtra("path", "earth");
+		}
+		startActivity(i);
+	}
 }
