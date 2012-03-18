ca96173cee95c2cdc108e0bccdb3291f5f01bbb6
Łukasz Anwajler <lukasz@anwajler.com>
2012-03-18 15:56:38 +0100

    Added FB support.

--- src/pl/org/kopernik/app/KopernikActivity.java
+++ src/pl/org/kopernik/app/KopernikActivity.java
@@ -5,19 +5,34 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class KopernikActivity extends Activity {
 	Context ctx;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        ctx = this;
-    }
-    
-    public void goToExhibit(View v) {
-    	Intent i = new Intent(ctx, PathActivity.class);
-    	startActivity(i);
-    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		ctx = this;
+
+		Intent i = getIntent();
+		if (i != null) {
+			Bundle extras = i.getExtras();
+			if (extras != null) {
+				if (extras.containsKey("showToast")) {
+					Toast.makeText(
+							getApplicationContext(),
+							"Informacja o przebytej ścieżce została opublikowana na Facebooku!",
+							Toast.LENGTH_LONG).show();
+				}
+			}
+		}
+	}
+
+	public void goToExhibit(View v) {
+		Intent i = new Intent(ctx, PathActivity.class);
+		startActivity(i);
+	}
 }
