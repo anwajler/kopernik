25ea2b662825e5584873aef4ae5ab5ecdcdff18d
Maciej Sawicki <maciej.sawicki@polidea.pl>
2012-03-18 16:38:18 +0100

    -added navigation between points

--- src/pl/org/kopernik/app/ExhibitActivity.java
+++ src/pl/org/kopernik/app/ExhibitActivity.java
@@ -1,5 +1,9 @@
 package pl.org.kopernik.app;
 
+import pl.org.kopernik.json.Exhibit;
+import pl.org.kopernik.json.Path;
+import pl.org.kopernik.json.Quize;
+import pl.org.kopernik.json.WaterPath;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -8,23 +12,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class ExhibitActivity extends Activity {
 	Context ctx;
+	WaterPath waterPath = KopernikApplication.waterPath;
+	TextView question;
+	Exhibit exhibit;
+	int exhibitSize;
+	int exhibitbNumber;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exhibit);
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        exhibitbNumber = (Integer) b.get("exhibitNumber");
+        exhibit = waterPath.path.exhibits.get(exhibitbNumber);
+        exhibitSize = waterPath.path.exhibits.size();
+        
+        question = (TextView) findViewById(R.id.question);
+        question.setText(exhibit.question.question_text);
         ctx = this;
     }
     
+    private boolean isLast(){
+    	return (exhibitSize - 1) == exhibitbNumber ? true : false;
+    }
+    
     public void goFurther(View v) {
-    	Intent i = new Intent(ctx, PathSummaryActivity.class);
-    	startActivity(i);
-    	Log.d("goFurther", KopernikApplication.waterPath.path.exhibits.get(0).name);
+    	if(isLast()){
+    		Intent i = new Intent(ctx, PathSummaryActivity.class);
+    		startActivity(i);
+    		Log.d("goFurther", KopernikApplication.waterPath.path.exhibits.get(0).name);
+    	} else{
+    	   	Intent i = new Intent(ctx, ExhibitActivity.class);
+        	i.putExtra("exhibitNumber", ++exhibitbNumber);
+        	startActivity(i); 
+    	}
     }
     
     public void goToDetails(View v) {
